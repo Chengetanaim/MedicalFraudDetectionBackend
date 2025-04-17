b6350125ca273547965631acf780e7568e5c5352
chengetanai <mukanhairic@gmail.com>
2025-04-17 16:16:44 +0200

    bb

--- main.py
+++ main.py
@@ -68,38 +68,38 @@ def predict_medical_insurance_claims(medical_insurance_data: MedicalInsuranceCre
     print("Loaded model type:", type(model))
     print("Loaded model repr:", repr(model))
 
-    try:
+    # try:
 
 
-        prediction = model(
-        RenalDiseaseIndicator=medical_insurance_data.RenalDiseaseIndicator,
-        ChronicDiseaseIndex=medical_insurance_data.ChronicDiseaseIndex,
-        InscClaimAmtReimbursed=medical_insurance_data.InscClaimAmtReimbursed,
-        DeductibleAmtPaid=medical_insurance_data.DeductibleAmtPaid,
-        IPAnnualReimbursementAmt=medical_insurance_data.IPAnnualDeductibleAmt,
-        OPAnnualReimbursementAmt=medical_insurance_data.OPAnnualDeductibleAmt,
-        IPAnnualDeductibleAmt=medical_insurance_data.IPAnnualDeductibleAmt,
-        OPAnnualDeductibleAmt=medical_insurance_data.OPAnnualDeductibleAmt,
-        treatment_intensity_score=medical_insurance_data.treatment_intensity_score
-    )
-    except Exception as e:
-        print("Failed to predict:", e)
-        raise HTTPException(status_code=400, detail=f"Failed to predict: {e}")
-    medical_insurance = MedicalInsurance( RenalDiseaseIndicator=medical_insurance_data.RenalDiseaseIndicator,
-    ChronicDiseaseIndex=medical_insurance_data.ChronicDiseaseIndex,
-    InscClaimAmtReimbursed=medical_insurance_data.InscClaimAmtReimbursed,
-    DeductibleAmtPaid=medical_insurance_data.DeductibleAmtPaid,
-    IPAnnualReimbursementAmt=medical_insurance_data.IPAnnualDeductibleAmt,
-    OPAnnualReimbursementAmt=medical_insurance_data.OPAnnualDeductibleAmt,
-    IPAnnualDeductibleAmt=medical_insurance_data.IPAnnualDeductibleAmt,
-    OPAnnualDeductibleAmt=medical_insurance_data.OPAnnualDeductibleAmt,
-    treatment_intensity_score=medical_insurance_data.treatment_intensity_score,
-    prediction=prediction)
+    #     prediction = model(
+    #     RenalDiseaseIndicator=medical_insurance_data.RenalDiseaseIndicator,
+    #     ChronicDiseaseIndex=medical_insurance_data.ChronicDiseaseIndex,
+    #     InscClaimAmtReimbursed=medical_insurance_data.InscClaimAmtReimbursed,
+    #     DeductibleAmtPaid=medical_insurance_data.DeductibleAmtPaid,
+    #     IPAnnualReimbursementAmt=medical_insurance_data.IPAnnualDeductibleAmt,
+    #     OPAnnualReimbursementAmt=medical_insurance_data.OPAnnualDeductibleAmt,
+    #     IPAnnualDeductibleAmt=medical_insurance_data.IPAnnualDeductibleAmt,
+    #     OPAnnualDeductibleAmt=medical_insurance_data.OPAnnualDeductibleAmt,
+    #     treatment_intensity_score=medical_insurance_data.treatment_intensity_score
+    # )
+    # except Exception as e:
+    #     print("Failed to predict:", e)
+    #     raise HTTPException(status_code=400, detail=f"Failed to predict: {e}")
+    # medical_insurance = MedicalInsurance( RenalDiseaseIndicator=medical_insurance_data.RenalDiseaseIndicator,
+    # ChronicDiseaseIndex=medical_insurance_data.ChronicDiseaseIndex,
+    # InscClaimAmtReimbursed=medical_insurance_data.InscClaimAmtReimbursed,
+    # DeductibleAmtPaid=medical_insurance_data.DeductibleAmtPaid,
+    # IPAnnualReimbursementAmt=medical_insurance_data.IPAnnualDeductibleAmt,
+    # OPAnnualReimbursementAmt=medical_insurance_data.OPAnnualDeductibleAmt,
+    # IPAnnualDeductibleAmt=medical_insurance_data.IPAnnualDeductibleAmt,
+    # OPAnnualDeductibleAmt=medical_insurance_data.OPAnnualDeductibleAmt,
+    # treatment_intensity_score=medical_insurance_data.treatment_intensity_score,
+    # prediction=prediction)
     
-    session.add(medical_insurance)
-    session.commit()
-    session.refresh(medical_insurance)
-    print(prediction)
+    # session.add(medical_insurance)
+    # session.commit()
+    # session.refresh(medical_insurance)
+    # print(prediction)
     
     return type(model), repr(model)
 
